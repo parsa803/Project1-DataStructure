972e5c3dc8e8e59d33359a1a0e1966e6c6a911c8
Parsa <mhmdparsahsny@gmail.com>
2025-11-30 13:35:44 +0330

    normalesh kardam ghorbon agha

--- main.cpp
+++ main.cpp
@@ -20,3 +20,62 @@ struct TreeNode {
         delete right;
     }
 };
+
+bool isSign(char c){
+    return c=='+' || c=='-';
+}
+
+string normalizer(string s) {
+    string out;
+
+    for(int i=0;i<s.size();){
+        if(isSign(s[i])){
+            int j=i;
+            while(j<s.size() && isSign(s[j])) 
+                j++;
+
+            bool bol = false;
+            int p = out.size()-1;
+
+            while(p>=0 && out[p]==' ') 
+                p--;
+
+            if(p<0 || out[p]=='(') 
+                bol = true;
+
+            if(bol){
+                int c=0;
+                for(int k=i;k<j;k++)   
+                    if(s[k]=='-') 
+                        c++;
+
+                if(c%2==1) 
+                    out.push_back('-');
+            } else {
+                int c=0;
+                for(int k=i;k<j;k++) 
+                    if(s[k]=='-') 
+                        c++;
+                if(c%2==1) 
+                    out+="-";
+                else 
+                    out+="+";
+            }
+            i=j;
+        }
+        else{
+            out.push_back(s[i]);
+            i++;
+        }
+    }
+
+    return out;
+}
+
+int main(){
+    string user_input;
+    getline(cin, user_input);
+    user_input = normalizer(user_input);
+
+    cout << user_input;
+}
