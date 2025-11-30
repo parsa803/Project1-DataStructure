7d533fd1bb93fe939e1a0365a4614ec2d13efc6c
Iliya-Ebrahimi <iliyaebrahimiwork1000@gmail.com>
2025-11-30 16:01:52 +0330

    add Tokenizer function
    This function is for separating tokens based on project rules.

--- main.cpp
+++ main.cpp
@@ -24,13 +24,13 @@ bool isSign(char c){
     return c=='+' || c=='-';
 }
 
-string normalizer(string s) {
+string normalizer(string expr) {
     string out;
 
-    for(int i=0;i<s.size();){
-        if(isSign(s[i])){
+    for(int i=0;i<expr.size();){
+        if(isSign(expr[i])){
             int j=i;
-            while(j<s.size() && isSign(s[j])) 
+            while(j<expr.size() && isSign(expr[j])) 
                 j++;
 
             bool bol = false;
@@ -45,7 +45,7 @@ string normalizer(string s) {
             if(bol){
                 int c=0;
                 for(int k=i;k<j;k++)   
-                    if(s[k]=='-') 
+                    if(expr[k]=='-') 
                         c++;
 
                 if(c%2==1) 
@@ -53,7 +53,7 @@ string normalizer(string s) {
             } else {
                 int c=0;
                 for(int k=i;k<j;k++) 
-                    if(s[k]=='-') 
+                    if(expr[k]=='-') 
                         c++;
                 if(c%2==1) 
                     out+="-";
@@ -63,7 +63,7 @@ string normalizer(string s) {
             i=j;
         }
         else{
-            out.push_back(s[i]);
+            out.push_back(expr[i]);
             i++;
         }
     }
@@ -71,10 +71,45 @@ string normalizer(string s) {
     return out;
 }
 
+// Tokenizer
+vector<string> tokenize(const string& expr) {
+    vector<string> tokens;
+    string token;
+    for (size_t i = 0; i < expr.size(); ++i) {
+        char c = expr[i];
+        if (isspace(c)) continue;
+
+        if (isdigit(c) || c == '.') {
+            token = "";
+            while (i < expr.size() && (isdigit(expr[i]) || expr[i] == '.')) {
+                token += expr[i++];
+            }
+            --i;
+            tokens.push_back(token);
+        }
+        else if (isalpha(c)) {
+            token = "";
+            while (i < expr.size() && (isalnum(expr[i]) || expr[i] == '_')) {
+                token += expr[i++];
+            }
+            --i;
+            tokens.push_back(token);
+        }
+        else if (c == '+' || c == '-' || c == '*' || c == '/' || c == '^' || c == '(' || c == ')' || c == '√') {
+            tokens.push_back(string(1, c));
+        }
+        else {
+            throw runtime_error("Invalid character: " + string(1, c));
+        }
+    }
+    return tokens;
+}
+
 int main(){
     string user_input;
     getline(cin, user_input);
     user_input = normalizer(user_input);
-
-    cout << user_input;
+    vector<string>tokens = tokenize(user_input);
+    cout << user_input << endl;
+    for (int i = 0; i < tokens.size(); i++) cout << tokens[i] << " ";
 }
